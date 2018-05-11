ac9e9a4aba6fb78f8d44d29020fee034501f3f16
limberchq <mig4121@gmail.com>
2018-05-11 09:21:52 -0400

    implemetamos vue, modificamos  ExampleComponent.vue

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -10,11 +10,13 @@
     <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
     <link rel="shortcut icon" href="img/favicon.png">
     <title>Sistema Ventas - IncanatoIT</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Icons -->
     <link href="css/plantilla.css" rel="stylesheet">
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+    <div id="app">
     <header class="app-header navbar">
         <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
           <span class="navbar-toggler-icon"></span>
@@ -149,15 +151,16 @@
         <!-- /Fin del contenido principal -->
     </div>
 
-    
-
+     </div>
     <footer class="app-footer">
         <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
         <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
     </footer>
 
     <!-- Bootstrap and necessary plugins -->
+    <script src="js/app.js"></script>
     <script src="js/plantilla.js"></script>
+
 </body>
 
 </html>
