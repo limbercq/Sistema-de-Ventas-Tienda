f7ec193ba176dbf9269b76f4aa580ef9b22b13c9
limbercq <mig4121@hotmail.com>
2018-06-01 11:47:20 -0400

    implementamos la vista de categoria y modificacion a rutas

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -27,7 +27,7 @@ class CategoriaController extends Controller
      */
     public function store(Request $request)
     {   
-        $categoria =new Categoria()
+        $categoria =new Categoria();
         $categoria->nombre = $request->nombre;
         $categoria->descripcion = $request->descripcion;
         $categoria->condicion = '1';
