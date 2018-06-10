5b7c3a56faa0a81634333911874ba27ae2cf5f88
limbercq <mig4121@hotmail.com>
2018-06-10 16:47:56 -0400

    se implemento el modelo persona

---
+++ app/Persona.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{   /* se inclute el nombre de las columnas que van a recibir valores en nuestra tabla de personas de la base de datos*/
+    protected $fillable = ['nombre','tipo_documento','num_documento','dirrecion','telefono','email']
+}
