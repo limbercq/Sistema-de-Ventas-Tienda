5dfb5cd536a195f063006a9f29f2ebd7ae1798f5
limbercq <mig4121@hotmail.com>
2018-06-10 16:30:26 -0400

    se implento un generador de barras con vue-barcode

--- resources/assets/js/components/Articulo.vue
+++ resources/assets/js/components/Articulo.vue
@@ -118,6 +118,9 @@
                                     <label class="col-md-3 form-control-label" for="text-input">Codigo</label>
                                     <div class="col-md-9">
                                         <input type="text" v-model="codigo" class="form-control" placeholder="Codigo de barras">
+                                        <barcode :value="codigo" :options="{ format: 'EAN-13'}">
+                                            Generando codigo de barras.
+                                        </barcode>
                                     </div>
                                 </div>                            
                                 <div class="form-group row">
@@ -169,6 +172,7 @@
 </template>
 
 <script>
+import VueBarcode from 'vue-barcode';
     export default {
         data (){
             return{
@@ -199,6 +203,9 @@
                 buscar : '',
                 arrayCategoria : []
             }
+        },
+        components: {
+            'barcode': VueBarcode
         },
         computed:{
             isActived : function(){
