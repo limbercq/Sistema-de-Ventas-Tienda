02bf2e161ecaa857be8d80379059f4de98abe729
limbercq <mig4121@hotmail.com>
2018-06-11 15:54:54 -0400

    creamos la tabla proveedores en la bd

---
+++ database/migrations/2018_06_11_194312_create_proveedores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            //llave foranea 
+            $table->integer('id')->unsigned();
+            $table->string('contacto',50)->nullable();
+            $table->string('telefono_contacto',50)->nullable();    
+            
+            //hacer referencias y tipo de elminacion en cascada
+            $table->foreign('id')->references('id')->on('personas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
