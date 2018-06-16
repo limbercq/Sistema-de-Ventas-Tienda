08e7ffd73b5b8a65c1bbb24f1b835f832f8afade
limbercq <mig4121@hotmail.com>
2018-06-16 16:40:50 -0400

    se agrego controller, rutas a rol

---
+++ app/Http/Controllers/RolController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Rol;
+
+class RolController extends Controller
+{
+    public function index(Request $request)
+    {   // seguridad
+        if(!$request->ajax()) return redirect('/');
+        //listar todos los registros de la tabla categoria
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+
+        if($buscar ==''){
+            $roles = Rol::orderBy('id', 'desc')->paginate(3);
+        }
+        else{
+            $roles = Rol::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(5);
+        }
+      
+        return [
+            'pagination' => [
+                'total'        => $roles->total(),
+                'current_page' => $roles->currentPage(),
+                'per_page'     => $roles->perPage(),
+                'last_page'    => $roles->lastPage(),
+                'from'         => $roles->firstItem(),
+                'to'           => $roles->lastItem(),
+
+            ],
+            'roles' => $roles
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,12 @@ Route::post('/cliente/registrar', 'ClienteController@store');
 
 Route::put('/cliente/actualizar', 'ClienteController@update');
 
+
 Route::get('/proveedor', 'ProveedorController@index');
 
 Route::post('/proveedor/registrar', 'ProveedorController@store');
 
 Route::put('/proveedor/actualizar', 'ProveedorController@update');
+
+
+Route::get('/rol', 'RolController@index');
